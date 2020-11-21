432af15f285ab825d9694c4884c30f2f7fcb8d5f
John Smith <Doctorblue.king@gmail.com>
2020-11-21 10:11:16 +0700

    Update Sever and Gradle

--- rest-api-example-server/note_model.py
+++ rest-api-example-server/note_model.py
@@ -1,17 +1,14 @@
 class NoteModel:
     def __init__(
-        self, note_id=0, title="", description="", img_path="/static/avatar.png"
+        self, note_id=0, title="", description=""
     ):
         self.title = title
         self.description = description
         self.id = note_id
-        self.img_path = img_path
 
     def set_data(self, data):
         self.title = data["title"]
         self.description = data["description"]
-        if data["imgPath"] is not None:
-            self.img_path = data["imgPath"]
         return self
 
     def to_json(self, ip_address):
@@ -19,16 +16,9 @@ class NoteModel:
             "id": self.id,
             "title": self.title,
             "description": self.description,
-            "imgPath": self.check_path(ip_address, self.img_path),
         }
         return data
 
-    def check_path(self, ip_address, path):
-        if ip_address not in path:
-            return "http://" + ip_address + path
-        else:
-            return path
 
-
-def createNoteModel(note_id, data, img_path):
-    return NoteModel(note_id, data["title"], data["description"], data["imgPath"])
+def createNoteModel(note_id, data):
+    return NoteModel(note_id, data["title"], data["description"])
